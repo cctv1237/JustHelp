f5184f13c73cb171c1e497d1645ffe64f192f564
LongFan <cctv1237@live.com>
2018-09-01 13:13:25 +0800

    Initial Commit

---
+++ JustHelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JustHelp
+//
+//  Created by LongFan on 2018/9/1.
+//  Copyright © 2018 Long Fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
